27ea15d002096dc234a5d52e3ceb267a3c935e2e
Niharika Rakhewar <111850896+NiyaRakhewar@users.noreply.github.com>
2023-07-23 02:34:51 +0530

    update ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Card } from "./Card";
-
+import Tilt from "react-parallax-tilt";
 function App() {
   return (
     <div className="App bg-gray-900 h-screen w-screen relative overflow-hidden flex flex-col justify-center items-center gap-2">
-      <div className="h-30-r w-32-r bg-gradient-to-r  from-red-400 via-pink-500 to-purple-500 rounded-full absolute left-3/4 -top-56 animate-pulse "></div>
-      <div className="h-30-r w-30-r bg-gradient-to-r from-green-400 to-blue-500 rounded-full absolute top-3/4 right-3/4 animate-pulse flex flex-col justify-center items-center "></div>
+      <Tilt className="h-30-r w-32-r bg-gradient-to-r  from-red-400 via-pink-500 to-purple-500 rounded-full absolute left-3/4 -top-56 animate-pulse "></Tilt>
+
+      <Tilt className="h-30-r w-30-r bg-gradient-to-r  to-blue-500  from-green-400 rounded-full absolute top-3/4 right-3/4 animate-pulse flex flex-col justify-center items-center "></Tilt>
+
       <Card />
     </div>
   );
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
+import Tilt from "react-parallax-tilt";
 
 export const Card = () => {
   const [input, setInput] = useState({
@@ -98,94 +99,95 @@ export const Card = () => {
 
   return (
     <>
-      <div className="container h-96 w-96 bg-white bg-opacity-10 relative z-2 rounded-2xl shadow-5xl border-opacity-30 backdrop-filter backdrop-blur-sm">
-        <div className="h-full flex flex-col justify-evenly items-center">
-          <div className="text-white font-poppins text-2xl tracking-widest font-bold">
-            Calculate Earnings
-          </div>
-          <div className="flex flex-col justify-center self-start mx-7 w-80">
-            <label className="text-white font-poppins text-2l tracking-widest">
-              Gross Earnings (Rs)
-            </label>
-            <input
-              className="py-1.5 px-2 mt-1 bg-slate-500 rounded-md  "
-              name="grossEarnings"
-              placeholder=" Gross Earnings ₹"
-              type="number"
-              value={input.grossEarnings}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="flex flex-row justify-center items-center gap-2 mx-10 w-80">
-            <div className="flex flex-col justify-center self-start w-80">
-              <label className="text-white font-poppins tracking-widest w-100">
-                Dalal Earnings
+      <Tilt>
+        <div className="container h-96 w-96 bg-white bg-opacity-10 relative z-2 rounded-2xl shadow-5xl border-opacity-30 backdrop-filter backdrop-blur-sm">
+          <div className="h-full flex flex-col justify-evenly items-center">
+            <div className="text-white font-poppins text-2xl tracking-widest font-bold">
+              Calculate Earnings
+            </div>
+            <div className="flex flex-col justify-center self-start mx-7 w-80">
+              <label className="text-white font-poppins text-2l tracking-widest">
+                Gross Earnings (Rs)
               </label>
               <input
-                className="w-40 py-1.5 px-2 mt-1 bg-slate-500 rounded-md"
-                name="dalalPercentage"
-                placeholder=" Dalal Earnings %"
+                className="py-1.5 px-2 mt-1 bg-slate-500 rounded-md  "
+                name="grossEarnings"
+                placeholder=" Gross Earnings ₹"
                 type="number"
-                value={input.dalalPercentage}
+                value={input.grossEarnings}
                 onChange={handleInputChange}
               />
             </div>
-            <div className="flex flex-col justify-center self-start w-80">
-              <label className="text-white font-poppins tracking-widest">
-                Trader Earnings
-              </label>
-              <input
-                className="py-1.5 px-2 mt-1 bg-slate-500 rounded-md w-40"
-                name="traderPercentage"
-                placeholder=" Trader Earnings %"
-                type="number"
-                value={input.traderPercentage}
-                onChange={handleInputChange}
-              />
+            <div className="flex flex-row justify-center items-center gap-2 mx-10 w-80">
+              <div className="flex flex-col justify-center self-start w-80">
+                <label className="text-white font-poppins tracking-widest w-100">
+                  Dalal Earnings
+                </label>
+                <input
+                  className="w-40 py-1.5 px-2 mt-1 bg-slate-500 rounded-md"
+                  name="dalalPercentage"
+                  placeholder=" Dalal Earnings %"
+                  type="number"
+                  value={input.dalalPercentage}
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className="flex flex-col justify-center self-start w-80">
+                <label className="text-white font-poppins tracking-widest">
+                  Trader Earnings
+                </label>
+                <input
+                  className="py-1.5 px-2 mt-1 bg-slate-500 rounded-md w-40"
+                  name="traderPercentage"
+                  placeholder=" Trader Earnings %"
+                  type="number"
+                  value={input.traderPercentage}
+                  onChange={handleInputChange}
+                />
+              </div>
             </div>
+            <div className="flex flex-row justify-center items-center gap-2 mx-10 w-80">
+              <div className="flex flex-col justify-center self-start w-80">
+                <label className="text-white font-poppins tracking-widest w-100">
+                  Misc Fees
+                </label>
+                <input
+                  className="py-1.5 px-2 mt-1 bg-slate-500 rounded-md w-40"
+                  name="miscFeesPercentage"
+                  placeholder=" Misc Fees %"
+                  type="number"
+                  value={input.miscFeesPercentage}
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className="referral-container">
+                <label className="text-white font-poppins tracking-widest w-100">
+                  Referral Earnings
+                </label>
+                <input
+                  className="py-1.5 px-2 mt-1 bg-slate-500 rounded-md w-40"
+                  name="referralPercentage"
+                  placeholder=" Referral %"
+                  type="number"
+                  value={input.referralPercentage}
+                  onChange={handleInputChange}
+                />
+              </div>
+            </div>
+            {errorMessage && (
+              <p className="text-red-500 mx-8 mt-2 w-100 font-poppins">
+                * {errorMessage}
+              </p>
+            )}
+            <button
+              className="cursor-pointer font-poppins rounded-full px-5 py-1.5 bg-white bg-opacity-50 hover:bg-white hover:bg-opacity-80 w-80 font-bold text-xl"
+              onClick={handleCalculation}
+            >
+              Calculate
+            </button>
           </div>
-          <div className="flex flex-row justify-center items-center gap-2 mx-10 w-80">
-            <div className="flex flex-col justify-center self-start w-80">
-              <label className="text-white font-poppins tracking-widest w-100">
-                Misc Fees
-              </label>
-              <input
-                className="py-1.5 px-2 mt-1 bg-slate-500 rounded-md w-40"
-                name="miscFeesPercentage"
-                placeholder=" Misc Fees %"
-                type="number"
-                value={input.miscFeesPercentage}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="referral-container">
-              <label className="text-white font-poppins tracking-widest w-100">
-                Referral Earnings
-              </label>
-              <input
-                className="py-1.5 px-2 mt-1 bg-slate-500 rounded-md w-40"
-                name="referralPercentage"
-                placeholder=" Referral %"
-                type="number"
-                value={input.referralPercentage}
-                onChange={handleInputChange}
-              />
-            </div>
-          </div>
-          {errorMessage && (
-            <p className="text-red-500 mx-8 mt-2 w-100 font-poppins">
-              * {errorMessage}
-            </p>
-          )}
-          <button
-            className="cursor-pointer font-poppins rounded-full px-5 py-1.5 bg-white bg-opacity-50 hover:bg-white hover:bg-opacity-80 w-80 font-bold text-xl"
-            onClick={handleCalculation}
-          >
-            Calculate
-          </button>
         </div>
-      </div>
-
+      </Tilt>
       {showOutput && (
         <div className="container h-40 w-96  relative z-2 rounded-2xl  border-opacity-30 backdrop-filter backdrop-blur-sm mt-8 flex flex-col justify-center items-center">
           <div className="text-white font-poppins text-xl mt-1 mb-2 tracking-widest font-bold">
